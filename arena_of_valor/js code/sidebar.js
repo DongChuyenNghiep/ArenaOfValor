700169df63f3c94f5136025f8812eebbe4be9d70
DongChuyenNghiep <tuan22000@student.hamk.fi>
2023-12-06 15:40:52 +0200

    Update sidebar.js

--- arena_of_valor/js code/sidebar.js
+++ arena_of_valor/js code/sidebar.js
@@ -1,4 +1,3 @@
-
 function w3_open() {
   document.getElementById("mySidebar").style.display = "block";
 }
@@ -7,12 +6,22 @@ function w3_close() {
   document.getElementById("mySidebar").style.display = "none";
 }
 
-document.addEventListener('click', function(event) {
-  var sidebar = document.getElementById('mySidebar');
-  var sidebarButton = document.getElementById('w3');
+document.body.addEventListener('touchend', function(event) {
+  var sidebar = document.getElementById("mySidebar");
+  var targetElement = event.target; // Element that triggered the event
 
-  // Check if the clicked element is not the sidebar or sidebar button
-  if (event.target !== sidebar && event.target !== sidebarButton) {
-      w3_close();
+  // Check if the clicked element is not within the sidebar
+  if (sidebar.style.display === "block" && !sidebar.contains(targetElement)) {
+    w3_close(); // Close the sidebar
   }
+});
+
+// Get all the links inside the sidebar
+var sidebarLinks = document.querySelectorAll("#mySidebar .linkElements");
+
+// Attach click event listeners to all sidebar links
+sidebarLinks.forEach(function(link) {
+  link.addEventListener('click', function() {
+    w3_close(); // Close the sidebar when a link is clicked
+  });
 });
