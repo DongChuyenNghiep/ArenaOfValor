381e52833feb00b66da97ba714e6502697ae1e86
DongChuyenNghiep <tuan22000@student.hamk.fi>
2023-12-06 13:44:57 +0200

    Update sidebar.js

--- arena_of_valor/js code/sidebar.js
+++ arena_of_valor/js code/sidebar.js
@@ -15,3 +15,13 @@ document.addEventListener('click', function(event) {
       w3_close();
   }
 });
+
+document.addEventListener('touchstart', function(event) {
+  var sidebar = document.getElementById('mySidebar');
+  var sidebarButton = document.getElementById('w3');
+
+  // Check if the touched element is not the sidebar or sidebar button
+  if (event.target !== sidebar && event.target !== sidebarButton) {
+      w3_close();
+  }
+});
