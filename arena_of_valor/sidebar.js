f9b244140fe6e25d4b9ed5f3d413029fbc6119e0
DongChuyenNghiep <tuan22000@student.hamk.fi>
2023-12-06 13:32:33 +0200

    update sidebar

--- arena_of_valor/sidebar.js
+++ arena_of_valor/sidebar.js
@@ -1,7 +1,17 @@
 function w3_open() {
-    document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("mySidebar").style.display = "block";
+}
+
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+}
+
+document.addEventListener('click', function(event) {
+  var sidebar = document.getElementById('mySidebar');
+  var sidebarButton = document.getElementById('w3');
+
+  // Check if the clicked element is not the sidebar or sidebar button
+  if (event.target !== sidebar && event.target !== sidebarButton) {
+      w3_close();
   }
-  
-  function w3_close() {
-    document.getElementById("mySidebar").style.display = "none";
-  }
+});
