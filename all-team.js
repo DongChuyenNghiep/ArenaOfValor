322a4a47acbec48b71482ed9bfb7ae507c28eeb6
DongChuyenNghiep <tuan22000@student.hamk.fi>
2023-12-09 21:54:35 +0200

    update pic

--- all-team.js
+++ all-team.js
@@ -1,6 +1,6 @@
-let SHEET_RANGE_TABLE = 'B70:AO80';
+let SHEET_RANGE_TABLE = 'B1:AN11';
 SHEET_ID = '1yhQbcmnQB52fu1PqlHPRNWOHmJwddS8J9EpIQqvJx2o'
-SHEET_TITLE = 'Form Responses 4'
+SHEET_TITLE = 'Sheet8'
 let FULL_URL_TABLE = `https://docs.google.com/spreadsheets/d/${SHEET_ID}/gviz/tq?sheet=${SHEET_TITLE}&range=${SHEET_RANGE_TABLE}`;
 
 fetch(FULL_URL_TABLE)
